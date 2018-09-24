c865b8119df91593c642f34b01073199714688f3
MJNeff18579 <MJNeff18579@CC121021.students.school.marmion.org>
2018-09-24 13:37:51 -0500

    Fixed Scanner issue.

--- src/madlibs/MadLibs.java
+++ src/madlibs/MadLibs.java
@@ -14,37 +14,38 @@ import java.util.Scanner;
 public class MadLibs {
 
     public static void main(String[] args) {
-        Scanner kboard = new Scanner(System.in);
+        Scanner txtrdr = new Scanner(System.in);
+        Scanner numrdr = new Scanner(System.in);
         
         System.out.println("Write a verb");
         
-        String verb1 = kboard.nextLine();
+        String verb1 = txtrdr.nextLine();
         System.out.println("Write an integer");
-        int int1 = kboard.nextInt();
+        int int1 = numrdr.nextInt();
         
         System.out.println("Write a person's name");
-        String name1 = kboard.nextLine();
+        String name1 = txtrdr.nextLine();
         
         System.out.println("Write an adverb");
-        String adverb1 = kboard.nextLine();
+        String adverb1 = txtrdr.nextLine();
         
         System.out.println("Write an animal");
-        String animal1 = kboard.nextLine();
+        String animal1 = txtrdr.nextLine();
         
         System.out.println("Write a past tense verb");
-        String pastverb1 = kboard.nextLine();
+        String pastverb1 = txtrdr.nextLine();
         
         System.out.println("Write a decimal");
-        double decimal1 = kboard.nextDouble();
+        double decimal1 = numrdr.nextDouble();
         
         System.out.println("Write an adjective");
-        String adjective1 = kboard.nextLine();
+        String adjective1 = txtrdr.nextLine();
         
         System.out.println("Write another integer");
-        int int2 = kboard.nextInt();
+        int int2 = numrdr.nextInt();
         
         System.out.println("Write a noun");
-        String noun1 = kboard.nextLine();
+        String noun1 = txtrdr.nextLine();
         
         System.out.println("test3");
         System.out.println("");
